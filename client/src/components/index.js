75270465aef2691c177679eefb493cfc9b124a6e
MFaiqKhan <faiqkhanthinkcentre@yahoo.com>
2022-03-02 11:31:37 +0500

    barrel exports for App.jsx

---
+++ client/src/components/index.js
@@ -0,0 +1,11 @@
+export { default as Loader } from './Loader';
+export { default as Navbar } from './Navbar';
+export { default as Footer } from './Footer';
+export { default as Services } from './Services';
+export { default as Transactions } from './Transactions';
+export { default as Welcome } from './Welcome';
+
+//https://medium.com/swlh/import-components-in-react-like-a-pro-b1340cb76a1b
+// and
+//https://dev.to/justmyrealname/organize-react-components-better-with-barrel-exports-4fno
+//exporting all the components into a single index.js file then importing this in App.jsx
