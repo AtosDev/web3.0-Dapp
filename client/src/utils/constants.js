f259a1f8b87911357e66879ba780542ff3d5cb2f
MFaiqKhan <faiqkhanthinkcentre@yahoo.com>
2022-03-02 11:32:42 +0500

    abi and contract address

---
+++ client/src/utils/constants.js
@@ -0,0 +1,8 @@
+import abi from './Transactions.json'; // import the abi of the contract
+
+
+
+export const contractABI =  abi.abi; // export the abi of the contract to be used in the smart contract code 
+export const contractAddress = '0x16D5aBa7632654010e21ce3CA1eeda3d19782278'; // Address on which the contract is deployed on the ropsten network.
+
+//Transactions deployed to: 0x16D5aBa7632654010e21ce3CA1eeda3d19782278 
